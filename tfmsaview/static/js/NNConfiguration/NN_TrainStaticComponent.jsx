adacd5b1ddc1bfcb42a3736397dc68be7e6db587
healess <healess1@gmail.com>
2016-11-18 04:53:24 -0500

    Add action for CNN and WDNN.

--- tfmsaview/static/js/NNConfiguration/NN_TrainStaticComponent.jsx
+++ tfmsaview/static/js/NNConfiguration/NN_TrainStaticComponent.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import StepArrowComponent from './../NNLayout/common/StepArrowComponent';
 import LabelByChartComponent from './TrainStatic/LabelByChartComponent'
+import ReportRepository from './../repositories/ReportRepository'
+import Api from './../utils/Api'
 
 export default class NN_TrainStaticComponent extends React.Component {
     constructor(props) {
         super(props);
+    }
+
+    callWdnnTrain(nnid){
+        this.props.reportRepository.postWdnnTrain(this.context.NN_ID).then((data) => {
+           // this.setState({data: data})
+        });
+    }
+
+    callWdnnEval(nnid){
+        this.props.reportRepository.postWdnnEval(this.context.NN_ID).then((data) => {
+        //    this.setState({data: data})
+        });
+    }
+
+    callCnnTrain(nnid){
+        this.props.reportRepository.postCnnTrain(this.context.NN_ID).then((data) => {
+      //      this.setState({data: data})
+        });
+    }
+
+    callCnnEval(nnid){
+        this.props.reportRepository.postCnnEval(this.context.NN_ID).then((data) => {
+        //    this.setState({data: data})
+        });
     }
 
     render() {
@@ -14,15 +40,15 @@ export default class NN_TrainStaticComponent extends React.Component {
                 <ul className="tabHeader">
                     <li className="current"><a href="#">CNN</a></li>
                     <li><a href="#">RNN</a></li>
-                    <li><a href="#">Fully</a></li>
-                    <div className="btnArea">
-                        <button type="button" className="img-btn save">Save</button>
-                        <button type="button" className="img-btn save">Error Check</button>
-                        <button type="button" className="img-btn save">Train</button>
                         <StepArrowComponent />
-                    </div>
                 </ul>
                  <div className="container tabBody">
+                <div className="btnArea">
+                        <button type="button" className="img-btn save" onClick={this.callWdnnTrain.bind(this)}>WDNN Eval</button>
+                        <button type="button" className="img-btn save" onClick={this.callWdnnEval.bind(this)}>WDNN Train</button>
+                        <button type="button" className="img-btn save" onClick={this.callCnnTrain.bind(this)}>CNN Eval</button>
+                        <button type="button" className="img-btn save" onClick={this.callCnnEval.bind(this)}>CNN Train</button>
+                </div>
                     <LabelByChartComponent />
                  </div>  
             </section>
@@ -30,4 +56,10 @@ export default class NN_TrainStaticComponent extends React.Component {
     }
 }
 
-                       
+NN_TrainStaticComponent.defaultProps = {
+    reportRepository: new ReportRepository(new Api())
+};     
+
+NN_TrainStaticComponent.contextTypes = {
+    NN_ID: React.PropTypes.string
+};
