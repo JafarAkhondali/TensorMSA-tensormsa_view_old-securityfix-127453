697bf1eefec5e1fbdddd1f178ba59cb25a0cf293
seungwookim <tmddno1@naver.com>
2016-11-24 03:47:30 +0000

    update : add animation on pie chart

--- tfmsaview/static/js/NNConfiguration/TrainStatic/LabelByChartComponent.jsx
+++ tfmsaview/static/js/NNConfiguration/TrainStatic/LabelByChartComponent.jsx
@@ -4,6 +4,11 @@ import * as d3 from "d3"
 export default class LabelByChartComponent extends React.Component {
     constructor(props) {
         super(props);
+        this.pie = {};
+        this.arcs = {};
+        this.vis = {};
+        this.arc = {};
+        this.currAngle = {};
         this.state = {
             chartSection : null
         };
@@ -17,11 +22,19 @@ export default class LabelByChartComponent extends React.Component {
         this.makeChart();
     }
 
+    componentDidUpdate() { 
+        const chartSection1 = [];
+        let section = d3.keys(this.props.data);
+        for (const numSection of section){
+            this.updateChart(numSection, 'L' + numSection, this.props.data);
+        }
+    }
+
     makeChart(){ 
         const chartSection1 = [];
         let section = d3.keys(this.props.data);
         for (const numSection of section){
-            this.createChart(numSection, this.props.data);
+            this.createChart(numSection, 'L' + numSection, this.props.data);
         }
     }
 
@@ -37,36 +50,69 @@ export default class LabelByChartComponent extends React.Component {
         this.setState({chartSection: chartSection1});   
     }
 
-    createChart(section, allData){
-            let data = allData[section];
+    createChart(dataSection, section, allData){
+            let data = allData[dataSection];
             var w = 400;
             var h = 400;
             var r = h/4;
             var color = d3.scale.category20c();
 
-            var vis = d3.select('#L'+ section).append("svg:svg").data([data]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ")");
-            var pie = d3.layout.pie().value(function(d){return d.value;});
+            this.vis[section] = d3.select('#' + section).append("svg:svg").attr("id", 'svg_' + dataSection).data([data]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ")");
+            this.pie[section] = d3.layout.pie().value(function(d){return d.value;});
+
 
             // declare an arc generator function
-            var arc = d3.svg.arc().outerRadius(r);
+            
+            var arc =  d3.svg.arc().outerRadius(r);
+            this.arc[section] = arc;
 
             // select paths, use arc generator to draw
-            var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
-            arcs.append("svg:path")
-                .attr("fill", function(d, i){
-                    return color(i);
-                })
-                .attr("d", function (d) {
-                    return arc(d);
-                });
+            let temp = this.currAngle;
+            this.arcs[section] = this.vis[section].selectAll("g.slice").data(this.pie[section]).enter().append("svg:g").attr("class", "slice").attr('id', 'arcs_' + dataSection);
+            this.arcs[section].append("svg:path")
+                    .attr("fill", function(d, i){
+                        return color(i);
+                    })
+                    .attr("d", function (d) {
+                        return arc(d);
+                    })
+                    .each(function(d) {
+                        temp[section] = d;
+                    });
+            
+            this.currAngle[section] = temp[section]
 
             // add the text
-            arcs.append("svg:text").attr("transform", function(d){
-                        d.innerRadius = 0;
-                        d.outerRadius = r;
-                return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
-                return data[i].label;}
-                    );
+            this.arcs[section].append("svg:text").attr("transform", function(d){
+                            d.innerRadius = 0;
+                            d.outerRadius = r;
+                            return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
+                            return data[i].label;}
+                        );
+    }
+
+    updateChart(dataSection, section, allData){
+        /*redraw pattern*/
+        d3.select('#svg_' + dataSection).remove();
+        this.createChart(dataSection, section, allData);
+
+        /* static pattern*/
+        // let data = allData[dataSection];
+        // this.pie[section].value(function(d){d[data];});
+        // this.arcs[section].data(this.pie[section]);
+        // this.arcs[section].attr("d", this.arc[section])
+
+        /* animation pattern*/
+        // let currAngle = this.currAngle;
+        // let arc = this.arc[section];
+        // function arcTween(a){
+        //     var i = d3.interpolate(currAngle, a);
+        //     currAngle = i(0);
+        //     return function(t){
+        //         return arc(i(t));
+        //     }
+        // }
+        // this.arcs[section].transition().duration(100).attrTween("d", arcTween); 
     }
 
     render() {
