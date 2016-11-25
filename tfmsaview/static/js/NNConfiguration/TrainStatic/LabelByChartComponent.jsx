d8c7b184c492775a54cfa86e4c8b57ecb40faec8
healess <healess1@gmail.com>
2016-11-25 09:59:05 -0500

    Add pie chart animation for statics.

--- tfmsaview/static/js/NNConfiguration/TrainStatic/LabelByChartComponent.jsx
+++ tfmsaview/static/js/NNConfiguration/TrainStatic/LabelByChartComponent.jsx
@@ -1,40 +1,44 @@
 import React from 'react'
 import * as d3 from "d3"
+import dc from 'dc'
+import crossfilter from 'crossfilter'
+import '../../../node_modules/dc/dc.css'
 
 export default class LabelByChartComponent extends React.Component {
     constructor(props) {
         super(props);
-        this.pie = {};
-        this.arcs = {};
-        this.vis = {};
-        this.arc = {};
-        this.currAngle = {};
         this.state = {
             chartSection : null
         };
     }
 
     componentWillMount() {
+
+        console.log("Data 1: " );
+        console.log(this.props.data);
+
         this.makeChartDOM();
     }
 
     componentDidMount() { //after DOM make D3 Chart
+
+        console.log("Data 2: " );
+        console.log(this.props.data);
         this.makeChart();
     }
 
     componentDidUpdate() { 
-        const chartSection1 = [];
-        let section = d3.keys(this.props.data);
-        for (const numSection of section){
-            this.updateChart(numSection, 'L' + numSection, this.props.data);
-        }
+
+        console.log("Data 3: " );
+        console.log(this.props.data);
+                this.makeChart();
     }
 
     makeChart(){ 
         const chartSection1 = [];
         let section = d3.keys(this.props.data);
         for (const numSection of section){
-            this.createChart(numSection, 'L' + numSection, this.props.data);
+            this.createChart(numSection, this.props.data);
         }
     }
 
@@ -44,75 +48,27 @@ export default class LabelByChartComponent extends React.Component {
         for (const numSection of section){
             chartSection1.push(<dl className='data-box' key = {numSection}>
                                 <dt><span>{numSection}</span></dt>
-                                <dd id={'L'+numSection}></dd>
+                                <dd id={"_" + numSection}></dd>
                             </dl>);
         }
         this.setState({chartSection: chartSection1});   
     }
 
-    createChart(dataSection, section, allData){
-            let data = allData[dataSection];
-            var w = 400;
-            var h = 400;
-            var r = h/4;
-            var color = d3.scale.category20c();
+    createChart(section, allData){
+            let data = allData[section];
+            let chart = dc.pieChart('#_' + section);
+            let ndx           = crossfilter(data);
+            let Dimension  = ndx.dimension(function(d) {return d.label;});
+            let dataGroup = Dimension.group().reduceSum(function(d) {return d.value;});
 
-            this.vis[section] = d3.select('#' + section).append("svg:svg").attr("id", 'svg_' + dataSection).data([data]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ")");
-            this.pie[section] = d3.layout.pie().value(function(d){return d.value;});
-
-
-            // declare an arc generator function
-            
-            var arc =  d3.svg.arc().outerRadius(r);
-            this.arc[section] = arc;
-
-            // select paths, use arc generator to draw
-            let temp = this.currAngle;
-            this.arcs[section] = this.vis[section].selectAll("g.slice").data(this.pie[section]).enter().append("svg:g").attr("class", "slice").attr('id', 'arcs_' + dataSection);
-            this.arcs[section].append("svg:path")
-                    .attr("fill", function(d, i){
-                        return color(i);
-                    })
-                    .attr("d", function (d) {
-                        return arc(d);
-                    })
-                    .each(function(d) {
-                        temp[section] = d;
-                    });
-            
-            this.currAngle[section] = temp[section]
-
-            // add the text
-            this.arcs[section].append("svg:text").attr("transform", function(d){
-                            d.innerRadius = 0;
-                            d.outerRadius = r;
-                            return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
-                            return data[i].label;}
-                        );
-    }
-
-    updateChart(dataSection, section, allData){
-        /*redraw pattern*/
-        d3.select('#svg_' + dataSection).remove();
-        this.createChart(dataSection, section, allData);
-
-        /* static pattern*/
-        // let data = allData[dataSection];
-        // this.pie[section].value(function(d){d[data];});
-        // this.arcs[section].data(this.pie[section]);
-        // this.arcs[section].attr("d", this.arc[section])
-
-        /* animation pattern*/
-        // let currAngle = this.currAngle;
-        // let arc = this.arc[section];
-        // function arcTween(a){
-        //     var i = d3.interpolate(currAngle, a);
-        //     currAngle = i(0);
-        //     return function(t){
-        //         return arc(i(t));
-        //     }
-        // }
-        // this.arcs[section].transition().duration(100).attrTween("d", arcTween); 
+            chart
+                .width(300)
+                .height(200)
+                .innerRadius(0)
+                .dimension(Dimension)
+                .transitionDuration(1500) //add animation speed
+                .group(dataGroup);
+            chart.render();
     }
 
     render() {
