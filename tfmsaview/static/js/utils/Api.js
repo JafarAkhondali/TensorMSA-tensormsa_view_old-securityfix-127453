83e8005f722fb938447bb0148b7c5af1d09dffaf
healess <healess1@gmail.com>
2016-11-30 09:14:21 -0500

    Refactoring Api.js to ECMA 2015

--- tfmsaview/static/js/utils/Api.js
+++ tfmsaview/static/js/utils/Api.js
@@ -1,113 +1,109 @@
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
-
+ 
 import EnvConstants from './../constants/EnvConstants';
 
-function Api() {
+export default class Api{
+    get (url, params) {
+        console.log(EnvConstants.getApiServerUrl() + url + params);
+        return fetch(
+            EnvConstants.getApiServerUrl() + url + params,
+            {
+                method: 'GET',
+                mode: "cors",
+                headers: new Headers({
+                    'Accept': 'application/json'
+                })
+            }
+        ).then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            return json;
+        }).catch(function(e) {
+            console.log("An Error has occurred" + e);
+        });
+    };
 
+    post(url, params) {
+        console.log(EnvConstants.getApiServerUrl());
+        return fetch(
+            EnvConstants.getApiServerUrl() + url,
+            {
+                method: 'POST',
+                mode: "cors",
+                body: JSON.stringify(params),
+                headers: new Headers({
+                    'Accept': 'application/json'
+                })
+            }             
+        ).then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            return json;
+        }).catch(function(e) {
+            console.log("An Error has occurred :" +e);
+        });
+        };
+
+    put(url, params) {
+        console.log(EnvConstants.getApiServerUrl());
+        return fetch(
+            EnvConstants.getApiServerUrl() + url,
+            {
+                method: 'PUT',
+                mode: "cors",
+                body: JSON.stringify(params),
+                headers: new Headers({
+                    'Accept': 'application/json'
+                })
+            }
+        ).then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            return json;
+        }).catch(function() {
+            console.log("An Error has occurred");
+        });
+    };
+
+    delete  (url, params) {
+        console.log(EnvConstants.getApiServerUrl());
+        return fetch(
+            EnvConstants.getApiServerUrl() + url,
+            {
+                method: 'DELETE',
+                mode: "cors",
+                body: JSON.stringify(params),
+                headers: new Headers({
+                    'Accept': 'application/json'
+                })
+            }
+        ).then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            return json;
+        }).catch(function() {
+            console.log("An Error has occurred");
+        });
+    };
+
+    getJson  (url, params) {
+        return fetch(
+            url,
+            {
+                method: 'POST',
+                mode: "cors",
+                body: JSON.stringify(params),
+                headers: new Headers({
+                    'Accept': 'application/json'
+                })
+            }
+        ).then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            return json;
+        }).catch(function(e) {
+            console.log("An Error has occurred :" +e);
+        });
+    };
 }
-
-Api.prototype.get = function (url, params) {
-    console.log(EnvConstants.getApiServerUrl() + url + params)
-    return fetch(
-        EnvConstants.getApiServerUrl() + url + params,
-        {
-            method: 'GET',
-            mode: "cors",
-            headers: new Headers({
-                 'Accept': 'application/json'
-            })
-        }
-    ).then(function(response) {
-        return response.json();
-    }).then(function(json) {
-        return json;
-    }).catch(function(e) {
-        console.log("An Error has occurred" + e);
-    });
-};
-
-Api.prototype.post = function (url, params) {
-    console.log(EnvConstants.getApiServerUrl());
-    return fetch(
-        EnvConstants.getApiServerUrl() + url,
-        {
-            method: 'POST',
-            mode: "cors",
-            body: JSON.stringify(params),
-            headers: new Headers({
-                'Accept': 'application/json'
-            })
-        }             
-    ).then(function(response) {
-        return response.json();
-    }).then(function(json) {
-        return json;
-    }).catch(function(e) {
-        console.log("An Error has occurred :" +e);
-    });
-};
-
-Api.prototype.put = function (url, params) {
-    console.log(EnvConstants.getApiServerUrl());
-    return fetch(
-        EnvConstants.getApiServerUrl() + url,
-        {
-            method: 'PUT',
-            mode: "cors",
-            body: JSON.stringify(params),
-            headers: new Headers({
-                'Accept': 'application/json'
-            })
-        }
-    ).then(function(response) {
-        return response.json();
-    }).then(function(json) {
-        return json;
-    }).catch(function() {
-        console.log("An Error has occurred");
-    });
-};
-
-Api.prototype.delete = function (url, params) {
-    console.log(EnvConstants.getApiServerUrl());
-    return fetch(
-        EnvConstants.getApiServerUrl() + url,
-        {
-            method: 'DELETE',
-            mode: "cors",
-            body: JSON.stringify(params),
-            headers: new Headers({
-                'Accept': 'application/json'
-            })
-        }
-    ).then(function(response) {
-        return response.json();
-    }).then(function(json) {
-        return json;
-    }).catch(function() {
-        console.log("An Error has occurred");
-    });
-};
-
-Api.prototype.getJson = function (url, params) {
-    return fetch(
-        url,
-        {
-            method: 'POST',
-            mode: "cors",
-            body: JSON.stringify(params),
-            headers: new Headers({
-                'Accept': 'application/json'
-            })
-        }
-    ).then(function(response) {
-        return response.json();
-    }).then(function(json) {
-        return json;
-    }).catch(function(e) {
-        console.log("An Error has occurred :" +e);
-    });
-};
-
-export default Api;
